e6c46d58f267ba42c3945f6cf1197e10211364ee
v.monasteretskyy <v.monasteretskyy@gmail.com>
2019-11-18 14:35:55 +0200

    catalog

--- js/common.js
+++ js/common.js
@@ -14,6 +14,8 @@ $(document).ready(function () {
     $('.burger').click(function () {
         $(this).toggleClass('active');
         $('header.header .col-xl-7.col-lg-6').toggle();
+        $('.submenu').hide();
+        $('.header__menu>ul li span').removeClass('actaft');
     });
 
     if ($(window).width() < 992) {
@@ -37,7 +39,78 @@ $(document).ready(function () {
 
     // input slyled
     $(function () {
-        $('select').styler();
+        $('select, .filter__lists.radiostyled input[type="radio"], .check-formfield input').styler();
     });
     // input slyled - END
+
+    // Catalog filter
+    $('.filter__title').click(function () {
+        $(this).toggleClass('rotatearrow');
+        $(this).next('.filter__lists').slideToggle();
+    });
+    // Catalog filter end
+
+    // input mask
+    if ($('*').is('.form-section')) {
+        $('.email-input').inputmask({
+            mask: "*{1,20}[.*{1,20}][.*{1,20}][.*{1,20}]@*{1,20}[.*{2,6}][.*{1,2}]",
+            greedy: false,
+            onBeforePaste: function (pastedValue, opts) {
+                pastedValue = pastedValue.toLowerCase();
+                return pastedValue.replace("mailto:", "");
+            },
+            definitions: {
+                '*': {
+                    validator: "[0-9A-Za-z!#$%&'*+/=?^_`{|}~\-]",
+                    casing: "lower"
+                }
+            }
+        });
+
+        $(".phone-input").inputmask({ "mask": "(999) 999-9999" });
+    }
+    // input mask END
+
+    // num products + or -
+    // plus minus
+    function incrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal)) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal + 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(0);
+        }
+    }
+
+    function decrementValue(e) {
+        e.preventDefault();
+        var fieldName = $(e.target).data('field');
+        var parent = $(e.target).closest('div');
+        var currentVal = parseInt(parent.find('input[name=' + fieldName + ']').val(), 10);
+
+        if (!isNaN(currentVal) && currentVal > 0) {
+            parent.find('input[name=' + fieldName + ']').val(currentVal - 1);
+        } else {
+            parent.find('input[name=' + fieldName + ']').val(0);
+        }
+    }
+
+    $('.input-group').on('click', '.button-plus', function (e) {
+        incrementValue(e);
+    });
+
+    $('.input-group').on('click', '.button-minus', function (e) {
+        decrementValue(e);
+    });
+
+    // num products END
+
+    // remove row - buy page
+    $('.incard__remove').click(function () {
+        $(this).parents('.incard__row').fadeOut();
+    });
 });
